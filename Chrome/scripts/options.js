fbceb08ca20539cdcde61c5c97812f689839a2e7
Devin Rousso <dcrousso@gmail.com>
2015-01-09 21:15:05 -0700

    Added ability to select tab which will become active on idle

--- Chrome/scripts/options.js
+++ Chrome/scripts/options.js
@@ -1,10 +1,11 @@
 function save_options() {
-	var timeout = document.getElementById('timeout').value;
-	console.log(timeout);
+	var timeout = document.getElementById("timeout").value;
+	var tabID = document.getElementById("tabID").value;
 	chrome.storage.sync.set({
-		"timeout": parseInt(timeout)
+		"timeout": parseInt(timeout),
+		"tabID": parseInt(tabID)
 	}, function() {
-		var status = document.getElementById('status');
+		var status = document.getElementById("status");
 		status.style.opacity = 1;
 		setTimeout(function() {
 			status.style.opacity = 0;
@@ -13,10 +14,25 @@ function save_options() {
 }
 function restore_options() {
 	chrome.storage.sync.get({
-		"timeout": 15
+		"timeout": 15,
+		"tabID": 0
 	}, function(items) {
-		document.getElementById('timeout').value = parseInt(items.timeout);
+		document.getElementById("timeout").value = parseInt(items.timeout);
+		document.getElementById(items.tabID).selected = true;
 	});
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+
+var selectTab = document.getElementById("tabID");
+chrome.tabs.query({}, function(tabs) {
+	tabs.forEach(function(tab) {
+		var option = document.createElement("option");
+		option.id = tab.id;
+		option.value = tab.id;
+		option.text = tab.title;
+		selectTab.appendChild(option);
+	});
+});
+
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", save_options);
+
--- Chrome/scripts/timer.js
+++ Chrome/scripts/timer.js
@@ -1,8 +1,10 @@
-var timeout;
+var timeout, tabID;
 chrome.storage.sync.get({
-	"timeout": 15
+	"timeout": 15,
+	"tabID": 0
 }, function(items) {
 	timeout = items.timeout * 60;
+	tabID = items.tabID;
 	chrome.idle.setDetectionInterval(timeout);
 });
 chrome.storage.onChanged.addListener(function(changes) {
@@ -10,13 +12,20 @@ chrome.storage.onChanged.addListener(function(changes) {
 		timeout = changes.timeout.newValue * 60;
 		chrome.idle.setDetectionInterval(timeout);
 	}
+	if("tabID" in changes) {
+		tabID = changes.tabID.newValue;
+	}
 });
 chrome.idle.onStateChanged.addListener(function(newState) {
 	if(newState == "idle" || newState == "locked") {
-		chrome.tabs.query({index: 0}, function(tabs) {
-			if(tabs[0].active == false) {
-				chrome.tabs.update(tabs[0].id, {active: true, selected: true});
-			}
-		});
+		if(tabID == 0) {
+			chrome.tabs.query({index: 0}, function(tabs) {
+				if(tabs[0].active == false) {
+					chrome.tabs.update(tabs[0].id, {active: true, selected: true});
+				}
+			});
+		} else {
+			chrome.tabs.update(tabID, {active: true, selected: true});
+		}
 	}
 });
