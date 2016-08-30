0ecd61fa9f7a0bf41ce56c15bc0c483973e8c65c
Pavel Bashinsky <pbashinsky@yandex-team.ru>
2016-08-30 16:32:21 +0300

    Cycle through tabs

--- Chrome/scripts/timer.js
+++ Chrome/scripts/timer.js
@@ -18,12 +18,33 @@ chrome.storage.onChanged.addListener(function(changes) {
 });
 chrome.idle.onStateChanged.addListener(function(newState) {
 	if(newState == "idle" || newState == "locked") {
-		if(tabID == 0) {
+		if(tabID == -1) {
+			toggleToNextTab();
+			chrome.alarms.create("inactivityAlarm", {'when': Date.now() + timeout * 1000});
+		} else if(tabID == 0) {
 			chrome.tabs.query({index:0}, function(tabs) {
 				chrome.tabs.update(tabs[0].id, {active: true, selected: true});
 			});
 		} else {
 			chrome.tabs.update(tabID, {active: true, selected: true});
 		}
+	} else {
+		chrome.alarms.clear("inactivityAlarm", function() {});
 	}
 });
+chrome.alarms.onAlarm.addListener(function(alarm) {
+	if (alarm.name == 'inactivityAlarm') {
+		toggleToNextTab();
+		chrome.alarms.create("inactivityAlarm", {'when': Date.now() + timeout * 1000});
+	}
+});
+function toggleToNextTab() {
+	chrome.tabs.query({}, function(tabs) {
+		if (tabs.length == 0) return;
+		var current = tabs.findIndex(function(tab) {return tab.active;});
+		chrome.tabs.update(
+			tabs[current == tabs.length - 1 ? 0 : current + 1].id,
+			{active: true, selected: true}
+		);
+	});
+}
